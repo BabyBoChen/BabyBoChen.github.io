70009e0035f29feae463fc67c213c6e6471c1af2
BabyBo <35010886+BabyBoChen@users.noreply.github.com>
2024-02-07 09:51:25 +0800

    Update visitorCountController.mjs

--- scripts/visitorCountController.mjs
+++ scripts/visitorCountController.mjs
@@ -2,11 +2,12 @@ import { getVisitorCt } from "./visitorCount.mjs";
 
 /** @param {angular.IModule} app  */
 function visitorCountController(app) {
-    app.controller("visitorCountController", async function($scope) {
+    app.controller("visitorCountController", function($scope) {
         $scope.visitorCnt = 0;
-        let count = await getVisitorCt('https://bblj-my-dart.azurewebsites.net/');
-        $scope.visitorCnt = count;
-        console.log($scope.visitorCnt);
+        getVisitorCt('https://bblj-my-dart.azurewebsites.net/').then(function(count){
+            $scope.visitorCnt = count;
+            console.log($scope.visitorCnt);
+        });
     });
 }
 
